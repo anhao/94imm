2bb8abb40731706c513c52ed69cb211487fbd621
zhangxinyu <weifeizxy@gmail.com>
2018-12-18 10:34:37 +0800

    新增4个爬虫，增加分类功能，修复tag不能分页bug

--- images/tests.py
+++ images/tests.py
@@ -1,108 +1,19 @@
-from bs4 import BeautifulSoup
 import threading,pymysql,time,requests,os,urllib3
-requests.packages.urllib3.disable_warnings()
 
-class Spider():
-    # 定义全局页面url列表
-    page_url_list = []
-    # 定义具体各表情图片url列表
-    img_url_list = []
-    # 定义rlock进程锁
-    rlock = threading.RLock()
+def get_data():
+    db = pymysql.connect("127.0.0.1", "root", "fendou2009", "silumz")
+    cursor = db.cursor()
+    # sqltab="alter table images_page AUTO_INCREMENT=1"
+    # cursor.execute(sqltab)
+    sql="select * from images_page where id>=46460 and id<=46446"
+    cursor.execute(sql)
+    i = 50000
+    for data in cursor.fetchall():
+        sqlupdata="update images_page set id="+"'"+str(i)+"'"+"where id="+"'"+str(data[0])+"'"
+        cursor.execute(sqlupdata)
+        sqlopimg="update images_image set pageid="+"'"+str(i)+"'"+"where pageid="+"'"+str(data[0])+"'"
+        cursor.execute(sqlopimg)
+        i=i-1
+        print(i)
 
-    def __init__(self,page_number=10,img_path='imgdir',thread_number=5):
-        """
-        :param page_number: 抓去多少个页面，默认10
-        :param img_dir: 定义图片目录
-        :param thread_number:默认5个线程
-        """
-        self.spider_url = 'https://fenmimi.com/page/'
-        self.page_number = int(page_number)
-        self.img_path = img_path
-        self.thread_num = thread_number
-
-    def get_url(self):
-        """
-        创建image目录和生产pageurl列表
-        :return:
-        """
-        for i in range(1,self.page_number+1):
-            page = requests.get(self.spider_url + str(i), verify=False).text
-            soup = BeautifulSoup(page, "html.parser").find_all("div", class_="post-list-item")
-            for pages in soup:
-                page_url=pages.find("div",class_="item-title").find("a").get("href")
-                self.page_url_list.append(page_url)
-
-    def get_img_url(self):
-        db = pymysql.connect("127.0.0.1", "root", "fendou2009", "silumz")
-        cursor = db.cursor()
-        tagidlist=[]
-        taglist=[]
-        print(self.page_url_list)
-        for page_url in self.page_url_list:
-            page = requests.get(page_url, verify=False).text
-            title= BeautifulSoup(page, "html.parser").find("h1",class_="post-title").text
-            p = (title, "1", time.strftime('%Y-%m-%d', time.localtime(time.time())), "1", "1")
-            cursor.execute("INSERT INTO images_page (title,tagid,sendtime,typeid,firstimg) VALUES (%s,%s,%s,%s,%s)", p)
-            soup = BeautifulSoup(page, "html.parser").find_all("div", class_="post-content")
-            for img_arr in soup:
-                for tags in img_arr.find_all("p", class_="post-tags"):
-                    for tag_a in tags.find_all("a"):
-                        tag = tag_a.text
-                        taglist.append(tag)
-                        sqltag = "SELECT * FROM images_tag WHERE tag =" + "'" + tag + "'" + " limit 1;"
-                        isExiststag = cursor.execute(sqltag)
-                        if isExiststag != 1:
-                            cursor.execute("INSERT INTO images_tag (tag) VALUES (%s)", tag)
-                        cursor.execute("SELECT id FROM images_tag WHERE tag =" + "'" + tag + "'")
-                        for id in cursor.fetchall():
-                            tagidlist.append(id[0])
-                cursor.execute(
-                    "UPDATE images_page SET tagid = " + "'" + str(tagidlist) + "'" + " WHERE title=" + "'" + title + "'")
-                cursor.execute("SELECT * FROM images_page WHERE title =" + "'" + title + "'" + " limit 1;")
-                plist = cursor.fetchall()
-                for pid in plist:
-                    pageid = pid[0]
-                imgs = img_arr.find_all("img")
-                is_cover=1
-                for img in imgs:
-                    imgsrc = img.get("src")
-                    self.img_url_list.append(imgsrc)
-                    path = imgsrc.split("/")[-2] + "/" + imgsrc.split("/")[-1]
-                    imgp = pageid, self.img_path + path
-                    cursor.execute("INSERT INTO images_image (pageid,imageurl) VALUES (%s,%s)", imgp)
-                    if is_cover==1:
-                        cursor.execute("UPDATE images_page SET firstimg = " + "'" +self.img_path + path + "'" + " WHERE title=" + "'" + title + "'")
-                    is_cover += 1
-        db.close()
-
-
-    def down_img(self):
-        Spider.rlock.acquire()
-        for imgsrc in self.img_url_list:
-            path = imgsrc.split("/")[-2] + "/" + imgsrc.split("/")[-1]
-            isdata = os.path.exists("../"+self.img_path + imgsrc.split("/")[-2])
-            if isdata == False:
-                os.mkdir("../"+self.img_path + imgsrc.split("/")[-2])
-            with open("../"+self.img_path + path, "wb")as f:
-                f.write(requests.get(imgsrc, verify=False).content)
-        Spider.rlock.release()
-
-
-    def run(self):
-        # 启动thread_num个进程来爬去具体的img url 链接
-        # for th in range(self.thread_num):
-        #     add_pic_t = threading.Thread(target=self.get_img_url)
-        #     add_pic_t.start()
-
-        # 启动thread_num个来下载图片
-        for img_th in range(self.thread_num):
-            download_t = threading.Thread(target=self.down_img)
-            download_t.start()
-
-
-if __name__ == '__main__':
-    spider = Spider(page_number=590, img_path='/static/images/', thread_number=10)
-    spider.get_url()
-    spider.get_img_url()
-    spider.run()
+get_data()
--- silumz.sql
+++ silumz.sql
@@ -1,91 +1,73 @@
 /*
- Navicat Premium Data Transfer
+Navicat MySQL Data Transfer
 
- Source Server         : silumz
- Source Server Type    : MySQL
- Source Server Version : 50554
- Source Host           : 192.168.1.67:3306
- Source Schema         : silumz
+Source Server         : 94imm
+Source Server Version : 50724
+Source Host           : 144.34.255.74:3306
+Source Database       : silumz
 
- Target Server Type    : MySQL
- Target Server Version : 50554
- File Encoding         : 65001
+Target Server Type    : MYSQL
+Target Server Version : 50724
+File Encoding         : 65001
 
- Date: 10/12/2018 22:09:06
+Date: 2018-12-18 10:30:45
 */
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
 -- Table structure for django_migrations
 -- ----------------------------
 DROP TABLE IF EXISTS `django_migrations`;
-CREATE TABLE `django_migrations`  (
+CREATE TABLE `django_migrations` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `app` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `app` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
   `applied` datetime NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Table structure for images_image
 -- ----------------------------
 DROP TABLE IF EXISTS `images_image`;
-CREATE TABLE `images_image`  (
+CREATE TABLE `images_image` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `pageid` int(11) NOT NULL,
-  `imageurl` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `is_cover` tinyint(4) NULL DEFAULT NULL,
+  `imageurl` varchar(200) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 1709 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE=MyISAM AUTO_INCREMENT=195085 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Table structure for images_page
 -- ----------------------------
 DROP TABLE IF EXISTS `images_page`;
-CREATE TABLE `images_page`  (
+CREATE TABLE `images_page` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `typeid` int(11) NOT NULL,
   `sendtime` date NOT NULL,
-  `title` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `firstimg` int(11) NOT NULL,
-  `tagid` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `title` varchar(200) NOT NULL,
+  `firstimg` varchar(200) NOT NULL,
+  `tagid` varchar(200) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 167 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of images_page
--- ----------------------------
-INSERT INTO `images_page` VALUES (166, 1, '2018-12-10', '妩媚美少妇木木媚骨天成撩人心魄', 1673, '[92, 93, 94]');
-INSERT INTO `images_page` VALUES (165, 1, '2018-12-10', '知性丽人佑熙连体高叉让人魂牵梦绕', 1632, '[86, 105, 106, 107]');
+) ENGINE=MyISAM AUTO_INCREMENT=52039 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Table structure for images_tag
 -- ----------------------------
 DROP TABLE IF EXISTS `images_tag`;
-CREATE TABLE `images_tag`  (
+CREATE TABLE `images_tag` (
   `id` int(4) NOT NULL AUTO_INCREMENT,
-  `tag` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `tag` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 110 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE=MyISAM AUTO_INCREMENT=3353 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Table structure for images_type
 -- ----------------------------
 DROP TABLE IF EXISTS `images_type`;
-CREATE TABLE `images_type`  (
+CREATE TABLE `images_type` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `type` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of images_type
--- ----------------------------
-INSERT INTO `images_type` VALUES (1, '性感美女');
-INSERT INTO `images_type` VALUES (2, '靓丽腿模');
-INSERT INTO `images_type` VALUES (3, '清纯美女');
-
-SET FOREIGN_KEY_CHECKS = 1;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
